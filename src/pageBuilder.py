488d451691af67d017056786e6f16d359d8aef36
Saki Nawata <>
2020-09-05 12:00:02 +0900

    タブのページ切り替え部分，infoのパネルのバグ修正

--- src/pageBuilder.py
+++ src/pageBuilder.py
@@ -31,7 +31,6 @@ def head(title, stylesheets=[], scripts=[]):
 
 def header(jsonfile):
     html = "<header class='suns-serif'>"
-    """ NAVIもここで """
     data = json2dict(jsonfile)
     data["SIGNATURE"] = \
             a(data["SIGNATURE"]["link"],\
@@ -52,28 +51,28 @@ def header(jsonfile):
             + span(data["SIGNATURE"], myclass="info-item"),\
             )
     html+= "</header>"
-    return html;
+    return html
 
-def tabContainer(jsonfile):
-    tabs = json2dict(jsonfile, ordered=True)
-    # tab_item { "tabname": {page-function: "functionname", jsons: ['json1', 'json2']}}
-    tabhtml = ""
-    for tab in tabs:
-        tabhtml += addTab(tab, tabs[tab])
-    return div(\
-            div(tabhtml, myclass="tab_item"),\
-            myclass="tabs"\
-           )
+def tabItemContainer(tab_items, container_id):
+    return div(tab_items, myclass='tab_container', id=container_id)
 
-
-def rephrase(template_file, data_dict):
-    template_str = ""
-    with open(template_file) as f:
-        template_str = f.read()
-
-    for key in data_dict:
-        template_str = template_str.replace(key, data_dict[key], 1)
-    return template_str
+def addTab(tabname, id, name, tab_container_id, content, checked=False):
+    suffix = '_content'
+    style = "<style>"\
+        + "#" + id + suffix +"{display: none;}"\
+        + "#" + id + ":checked ~ #" + id + suffix\
+        + "{display: block;}"
+    style += "#" + id + ":checked + ." + "tab_item" + "{"\
+        + "background-color: white;"\
+        + "color: var(--my-black);"\
+        + "border: none;"\
+        + "}"
+        #+ "background-color: rgba(10, 10, 10, 80%);"
+    style += "</style>"
+    tab = input('radio', myclass='tab_checkbox', id=id, name=name, checked = checked)
+    tab += label(tabname, id, myclass='tab_item')
+    tabcontent = div(content, myclass='tab_content', id=id+suffix)
+    return {'tab_item': style + tab, 'tab_content': tabcontent}
 
 """ ******************************
 json
@@ -89,6 +88,15 @@ def json2dict(filename, ordered=False):
 """ ******************************
 tools
 ********************************* """
+def rephrase(template_file, data_dict):
+    template_str = ""
+    with open(template_file) as f:
+        template_str = f.read()
+
+    for key in data_dict:
+        template_str = template_str.replace(key, data_dict[key], 1)
+    return template_str
+
 def makedirs(path, fource = False):
     if not os.path.isdir(path):
         os.makedirs(path)
@@ -104,28 +112,31 @@ html parts
 def h1(content, myclass='', id=''):
     return "<h1" + attribute(myclass, id) + ">" + content + "</h1>"
 
-def input (type, myclass='', id='', title=''):
+def input (type, myclass='', id='', name='', title='', checked=False):
     html_str = "<input type='" + type + "'"
-    html_str += attribute(myclass, id)
+    html_str += attribute(myclass, id, name=name, checked=checked)
     html_str += ">"
     return html_str
 
 def label(label_str, for_id, myclass='', id='', title=''):
-    html =  "<label for=" + for_id + attribute(myclass, id, title) + ">"
+    html =  "<label for='" + for_id + "'" + attribute(myclass, id, title) + ">"
     html += label_str + "</label>"
     return html
 
-def attribute(myclass, id, title='', target=''):
+def attribute(myclass, id, title='', target='', name='', checked=False):
     html_str = ""
     if id != '':
         html_str += " id='" + id + "'"
     if myclass != '':
-        html_str += " class='" + myclass+ "'"
+        html_str += " class='" + myclass + "'"
+    if name != '':
+        html_str += " name='" + name + "'"
     if title != '':
-        html_str += " title='" + title+ "'"
+        html_str += " title='" + title + "'"
     if target != '':
-        html_str += " target=" + target+ "'"
-
+        html_str += " target=" + target + "'"
+    if checked:
+        html_str += " checked='checked'"
     return html_str
 
 def div(content, myclass='', id=''):
@@ -162,5 +173,11 @@ makedirs(OUTPUT_DIR, fource=True);
 data = {}
 data["HEAD"] = head("SimEpidemic", stylesheets=["../css/common.css"])
 data["HEADER"] = header(JSON_DIR + "info.json")
+tab1 = addTab('tabname', 'id', 'page_tabs', 'tabs', 'content')
+tab2 = addTab('tabname2', 'id2', 'page_tabs', 'tabs', 'content2', True)
+tabitems = tab1['tab_item'] + tab2["tab_item"]
+tab_contents = tab1["tab_content"] + tab2["tab_content"]
+data["MAIN"] = tabItemContainer(tabitems + tab_contents, 'tabs')
+data["MAIN"] += tab1["tab_content"] + tab2["tab_content"]
 with open(OUTPUT_DIR + "index.html", mode="w") as f:
     f.write(rephrase(TEMPLATE_DIR + "base.html", data))
