a6bf55b1360fb35038461a95b2b78c0a34701250
saki <>
2021-03-02 00:13:05 +0900

    分布以外のグラフに凡例をつけた．指標別についてはチェックボックスで表示切り替え可能． 見るに耐えなかったので言語切り替え用の文章は別ファイルへ． 要確認：検査崇敬の指標が累積値っぽい．getIndexesだと累積しか取れない？

--- SimEpidemic/script/GraphPopup.js
+++ SimEpidemic/script/GraphPopup.js
@@ -4,156 +4,70 @@
  */
 const popup = {};
 
-popup.stacked_keys = ["died","asymptomatic",  "symptomatic", "recovered", "vaccinated", "susceptible"];
-popup.index_keys = ["died","asymptomatic",  "symptomatic", "recovered", "vaccinated", "susceptible",
+popup.stacked_keys = ["died","asymptomatic", "symptomatic", "recovered", "vaccinated", "susceptible"];
+popup.index_keys = {
+    idx: ["died","asymptomatic", "symptomatic", "recovered", "vaccinated", "susceptible",
     "quarantineAsymptomatic", "quarantineSymptomatic",
     "tests", "testPositive", "testNegative",
-    "testAsSymptom", "testAsContact", "testAsSuspected"];
+    "testAsSymptom", "testAsContact", "testAsSuspected"]};
 popup.positiveRate_key = "testPositiveRate";
 /**
  * contacts(接触者数(人)/人日)は未実装
  */
 popup.distribution_keys = ["incubasionPeriod", "recoveryPeriod", "fatalPeriod", "infects"];
 
-popup.msg = {
-    update: {
-        "JA": "更新",
-        "EN": "Update"
-    },
-    stack: {
-        title: {
-            "JA": "全体構成変化",
-            "EN": "Whole"
-        },
-        indexes: {
-            died: {
-                "JA": "死亡者数",
-                "EN": "Died"
-            },
-            asymptomatic: {
-                "JA": "無症状感染者数",
-                "EN": "Asymptmatic"
-            },
-            symptomatic: {
-                "JA": "発症感染者数",
-                "EN": "Symptomatic"
-            },
-            recovered: {
-                "JA": "快復者数",
-                "EN": "Recovered"
-            },
-            susceptible: {
-                "JA": "未感染者数",
-                "EN": "Susceptible"
-            },
-            vaccinated: {
-                "JA": "ワクチン接種者数",
-                "EN": "Vaccinated"
-            }
-        }
-    },
-    index: {
-        title: {
-            "JA": "指標別変化",
-            "EN": "Indexes..."
-        },
-        indexes: {
-            died: {
-                "JA": "死亡者数",
-                "EN": "Died"
-            },
-            asymptomatic: {
-                "JA": "無症状感染者数",
-                "EN": "Asymptmatic"
-            },
-            symptomatic: {
-                "JA": "発症感染者数",
-                "EN": "Symptomatic"
-            },
-            recovered: {
-                "JA": "快復者数",
-                "EN": "Recovered"
-            },
-            susceptible: {
-                "JA": "未感染者数",
-                "EN": "Susceptible"
-            },
-            quarantineAsymptomatic: {
-                "JA": "無症状隔離数",
-                "EN": "QuarantineAsymptomatic"
-            },
-            quarantineSymptomatic: {
-                "JA": "有症状隔離数",
-                "EN": "QuarantineSymptomaticr"
-            },
-            tests: {
-                "JA": "検査数",
-                "EN": "Tests"
-            },
-            testPositive: {
-                "JA": "陽性者数",
-                "EN": "Test Positive"
-            },
-            testNegative: {
-                "JA": "陰性者数",
-                "EN": "testNegative"
-            },
-            testAsSymptom: {
-                "JA": "発症者検査数",
-                "EN": "Test as Symptomatic"
-            },
-            testAsContact: {
-                "JA": "接触者検査数",
-                "EN": "Test as Contact"
-            },
-            testAsSuspected: {
-                "JA": "擬症状者数",
-                "EN": "Test as Suspected"
-            }
-        }
-    },
-    testPositiveRate: {
-        title: {
-            "JA": "陽性率",
-            "EN": "Test Positive Rate"
-        },
-        indexes: {
-            testPositiveRate: {
-                "JA": "陽性率",
-                "EN": "Test Positive Rate"
-            }
-        }
-    },
-    distribution: {
-        title: {
-            "JA": "分布情報",
-            "EN": "Distribution"
-        },
-        indexes: {
-            incubasionPeriod: {
-                "JA": "潜伏期間",
-                "EN": "Incubation Period"
-            },
-            recoveryPeriod: {
-                "JA": "快復期間",
-                "EN": "Recovery Period"
-            },
-            fatalPeriod: {
-                "JA": "生存期間",
-                "EN": "Fataal Period"
-            },
-            infects: {
-                "JA": "伝染数",
-                "EN": "Infects"
-            },
-            contacts: {
-                "JA": "接触者数",
-                "EN": "Contacts"
-            }
-        }
+popup.msg = "";
+
+function indexFilter(obj, idx) {
+    console.log(idx);
+    const p = popup.index_keys.idx.findIndex(name => name == idx);
+    if(p < 0) {
+        console.log("Error: " + idx + 'is not found');
+        return;
+    }
+    const path_name = idx + '-path';
+    const path = document.getElementById(path_name);
+    if(obj.checked) {
+        const color = popup.colorScheme(popup.index_keys.idx);
+        obj.labels[0].innerHTML = "■";
+        popup.index_keys.visible[p] = true;
+        path.setAttribute('display', 'block');
+    } else {
+        popup.index_keys.visible[p] = false;
+        obj.labels[0].innerHTML = "<span style='color: black;display:inline-block; padding:0px 2.1px;'>×</span>";
+        path.setAttribute('display', 'none');
     }
 }
-popup.makeLegend = function (indexes) {
+
+popup.makeLegend = function (name, p_node_id, indexes, w, margin_x, margin_y, step = 20) {
+    const color = popup.colorScheme(indexes);
+    const id = popup.initChart(name, p_node_id, w, margin_y + step * indexes.length);
+    const legend_svg = d3.select(`#${id}`);
+    const legend_values = [];
+    for (let i = 0; i < indexes.length; i++) {
+        legend_values.push(popup.msg.index[indexes[i]][popup.getLanguage()]);
+    }
+    const legend = legend_svg.selectAll('.legends')
+        .data(legend_values)
+        .enter()
+        .append('g')
+        .attr("class", "legends")
+        .attr("transform", function(d, i) {
+            return `translate(0, ${margin_y + i * step})`;
+        });
+    //凡例の色付き四角
+    legend.append('rect').attr("x", 0).attr("y", 0)
+        .attr("width", 10).attr("height", 10)
+        .style("fill", function (d, i) {
+            return color(indexes[i]); });
+
+    //凡例の本文
+    legend.append('text').attr("x", 10).attr("y", 10)
+        .text(function (d, i) { return d;})
+            .attr("class", "textselected")
+            .style("text-anchor", "start")
+            .style("font-size", 11);
+    legend_svg.attr('transform', `translate(${popup.graph_width + margin_x}, 0)`);
 }
 
 popup.getData = function (w_id, idxes, callback, req_type ='index') {
@@ -206,29 +120,31 @@ popup.content = function (w_id) {
         { name: 'class', value: "command-button" },
         { name: 'onclick', value: "updateCharts();" }]);
 
-    addPartial("h2", popup.msg.stack.title[LANG]);
+    addPartial("h2", popup.msg.title.stack[LANG]);
     addPartial("div", "", [{ name: 'id', value: 'stack-graph' }]);
 
-    addPartial("h2", popup.msg.index.title[LANG]);
+    addPartial("h2", popup.msg.title.index[LANG]);
     addPartial("div", "", [{ name: 'id', value: 'index-graph' }]);
 
-    addPartial("h2", popup.msg.testPositiveRate.title[LANG]);
+    addPartial("h2", popup.msg.title.testPositiveRate[LANG]);
     addPartial("div", "", [{ name: 'id', value: 'rate-graph' }]);
 
-    addPartial("h2", popup.msg.distribution.title[LANG]);
+    addPartial("h2", popup.msg.title.distribution[LANG]);
     addPartial("div", "", [{ name: 'id', value: 'distribution' }]);
 
     wrapper.appendChild(main);
     return wrapper;
 }
 
-popup.graph_width = 500;
-
+popup.graph_width = 350;
+popup.legend_width = 250;
 popup.aspect = 0.56;
 
-popup.initChart = function (type, pnode_id) {
-    const w = popup.graph_width;
-    const h = w * popup.aspect;
+popup.initChart = function (type, pnode_id, w = 0, h = 0) {
+    if(w == 0 && h == 0) {
+        w = popup.graph_width + popup.legend_width + 10;
+        h = popup.graph_width * popup.aspect;
+    }
     const svg_id = type + '-svg';
     const svg_node = document.getElementById(svg_id);
     if(svg_node != null && svg_node != undefined) {
@@ -272,7 +188,8 @@ popup.drawStackedChart = function(dataset, scale_x, scale_y) {
         .selectAll("mylayers")
         .data(stacked_series)
         .join("path")
-        .style("fill", function(d) {return color(d.key)})
+        .style("fill", function(d) {
+            return color(d.key)})
         .attr("d", area_maker)
         .attr('transform', "translate(70, -30)");
     const x_axis = d3.select("#stack-svg")
@@ -284,30 +201,70 @@ popup.drawStackedChart = function(dataset, scale_x, scale_y) {
         .attr('transform', 'translate(70, -30)')
         .attr('fill', 'white')
         .call(d3.axisLeft(scale_y));
+    //凡例
+    popup.makeLegend('stacked-legends', 'stack-svg', popup.stacked_keys, popup.legend_width, 80, 30);
 }
 
 popup.colorScheme = function(domain) {
+    const mypalette = [
+        "dimgray",
+        "#c9fd51",
+        "#ff6f91",
+        "#ff9671",
+        "#68edcb",
+        "#00d6ec",
+        "mediumseagreen",
+        "crimson",
+        "#d65db1",
+        "#ffc75f",
+        "#2c73d2",
+        "indigo",
+        "teal",
+        "#845ec2"
+    ];
     const color = d3.scaleOrdinal()
         .domain(domain)
-        .range(d3.schemeSet2);
+        .range(mypalette);
     return color;
 }
 
 popup.drawIndexesChart = function (dataset, scale_x, scale_y) {
+    const color = popup.colorScheme(popup.index_keys.idx);
+    const p_id = 'index-graph';
+    const p_node = document.getElementById(p_id);
+    p_node.innerHTML = '';
+    const filter = document.createElement('div');
+    filter.setAttribute('style', 'display: flex; flex-wrap: wrap;');
+    let cboxes = "";
+
+    for(let idx of popup.index_keys.idx) {
+        const id = `${idx}-cbox`;
+        cboxes += "<div>";
+        cboxes += `<label for="${id}" style="border: solid 0.5px black; color: ${color(idx)};">■</label>`;
+        cboxes += `<input id="${id}" type="checkbox" style="display:none;" onchange="indexFilter(this, '${idx}');" checked="checked">`;
+        cboxes += '<span style = "display: inline-block; margin-left: 3px; margin-right: 10px;">'
+        cboxes += popup.msg.index[idx][popup.getLanguage()];
+        cboxes += "</span>";
+        cboxes += "</div>";
+    }
+    filter.innerHTML = cboxes;
+    p_node.appendChild(filter);
+
     const height = popup.graph_width * popup.aspect;
-    popup.initChart('index', 'index-graph');
+    popup.initChart('index', p_id);
     const day = dataset['days'];
-    const color = popup.colorScheme(popup.index_keys);
     const svg = d3.select("#index-svg");
     const line_maker  = d3.line()
         .defined(d => !isNaN(d))
         .y(function(d) { return scale_y(d);})
-        .x((d, i) => scale_x(day * (i / dataset[popup.index_keys[0]].length)));
+        .x((d, i) => scale_x(day * (i / dataset[popup.index_keys.idx[0]].length)));
     const group = svg.append("g");
-    for(let name of popup.index_keys) {
+    for(let p in popup.index_keys.idx) {
+        const name = popup.index_keys.idx[p];
         group.append("path")
             .datum(dataset[name])
             .join("path")
+            .attr('id', `${name}-path`)
             .attr('transform', 'translate(70, -30)')
             .attr("fill", "none")
             .style("stroke", color(name))
@@ -322,6 +279,9 @@ popup.drawIndexesChart = function (dataset, scale_x, scale_y) {
     const y_axis = svg.append("g")
         .attr('transform', 'translate(70, -30)')
         .call(d3.axisLeft(scale_y));
+
+    //凡例
+    //popup.makeLegend('index-legends', 'index-svg', popup.index_keys, popup.legend_width, 80, 30);
 }
 
 popup.drawPositiveRate = function (dataset, scale_x) {
@@ -343,7 +303,7 @@ popup.drawPositiveRate = function (dataset, scale_x) {
         .join("path")
         .attr('transform', 'translate(70, -30)')
         .attr("fill", "none")
-        .style("stroke", color(name))
+        .style("stroke", color(popup.positiveRate_key))
         .attr("stroke-width", 1)
         .attr("stroke-linejoin", "round")
         .attr("stroke-linecap", "round")
@@ -354,34 +314,39 @@ popup.drawPositiveRate = function (dataset, scale_x) {
     const y_axis = svg.append("g")
         .attr('transform', 'translate(70, -30)')
         .call(d3.axisLeft(scale_y));
+    //凡例
+    popup.makeLegend('rate-legends', 'rate-svg', [popup.positiveRate_key], popup.legend_width, 80, 30);
 }
 
 popup.drawDistribution = function(dataset) {
     console.log("distribution");
     console.log(dataset);
+    const height = popup.graph_width * popup.aspect;
 }
+
 
 popup.drawChart = function(dataset, popsize, type) {
     if(type == 'distribution') {
         popup.drawDistribution(dataset);
         return;
     }
-    const width = popup.graph_width;
-    const height = width * popup.aspect;
+    const height = popup.graph_width * popup.aspect;
 
     const day = dataset['days'];
     document.getElementById('day').innerText = day;
     const scale_x = d3.scaleLinear()
         .domain([0, day])
-        .range([0, width-100]);
+        .range([0, popup.graph_width]);
 
     const scale_y = d3.scaleLinear()
         .domain([0, popsize])
         .range([height, 50]);
 
     if(type == 'stack') popup.drawStackedChart(dataset, scale_x, scale_y);
-    if(type == 'index') popup.drawIndexesChart(dataset, scale_x, scale_y);
-    if(type == 'rate') popup.drawPositiveRate(dataset, scale_x, scale_y);
+    if(type == 'index') {
+        popup.drawIndexesChart(dataset, scale_x, scale_y);
+    }
+    if(type == 'rate') popup.drawPositiveRate(dataset, scale_x);
 }
 
 popup.getLanguage = function() {
@@ -407,7 +372,7 @@ function updateCharts() {
     const pop_size = popup.getPopSize();
     popup.getData(w_id, popup.stacked_keys,
         tool.callbackFunc(popup.drawChart, [pop_size, 'stack']));
-    popup.getData(w_id, popup.index_keys,
+    popup.getData(w_id, popup.index_keys.idx,
         tool.callbackFunc(popup.drawChart, [pop_size, 'index']));
     popup.getData(w_id, [popup.positiveRate_key],
         tool.callbackFunc(popup.drawChart, [pop_size, 'rate']));
@@ -420,15 +385,19 @@ function updateCharts() {
 console.log('load popup script');
 const lang = document.getElementById('lang');
 const w_id = document.getElementById('w_id');
-server.get((v)=>{
-    document.body.appendChild(
-        popup.content(w_id.innerText)
-    );
-    //console.log(v.populationSize);
-    tool.setHiddenValues({
-        'popsize': v.populationSize,
-        'w_id': w_id.innerText
-    }, "props");
-    updateCharts();
-}, "getParams?world=" + w_id.innerText, responseType="json");
+server.get((m)=>{
+    popup.msg = m;
+    server.get((p)=>{
+        popup.index_keys.visible = popup.index_keys.idx.map(()=>{return true;})
+        console.log(popup.index_keys);
+        document.body.appendChild(
+            popup.content(w_id.innerText)
+        );
+        tool.setHiddenValues({
+            'popsize': p.populationSize,
+            'w_id': w_id.innerText
+        }, "props");
+        updateCharts();
+    }, "getParams?world=" + w_id.innerText, responseType="json");
+}, "contents/stats_msg.json", responseType = "json");
 
